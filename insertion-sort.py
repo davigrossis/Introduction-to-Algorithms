119cbbc21688388dd3d41d4ef59fcf5bb0b4c0f1
davigrossis <davi.grossi.souza@gmail.com>
2026-07-11 15:00:04 -0300

    feat: insertion-sort.py

---
+++ insertion-sort.py
@@ -0,0 +1,16 @@
+A = [5,2,4,6,1,3]
+
+for j in range(1, len(A)):
+    key = A[j] 
+    print("valor da chave: ", key)
+    i = j-1
+    print("valor de i: ", i)
+    while i>=0 and A[i]>key:
+        A[i+1] = A[i]
+        print("valor de i pós while: ", i)
+        i = i-1
+    A[i+1] = key
+    print("valor de A[i+1]", A[i+1])
+    print("string final da ",j," interação:", A)
+print(A)
+    
